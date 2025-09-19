97f1a4007e2485e7c5122c8a19f3f42c293076e2
Shi-Shion <jamelaniasco7@gmail.com>
2025-09-20 02:34:13 +0800

    initial commit, another try

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import ColorChanger from './activity5/ColorChanger';
+import CounterApp from './activity5/CounterApp';
+
+export default function App() {
+  return (
+    <>
+      <CounterApp />
+      <ColorChanger />
+    </>
+  );
+}
